b49153023b42faa61e147f7328bbeafaf43b910f
Abhishek bhatt <104866090+abhishekbhattt@users.noreply.github.com>
2024-04-12 00:16:46 +0530

    creating registration form using HTML ,CSS,JS

---
+++ script.js
@@ -0,0 +1,11 @@
+function sendMail() {
+  let parms = {
+    name: document.getElementById("name").value,
+    email: document.getElementById("email").value,
+    subject: document.getElementById("subject").value,
+    message: document.getElementById("message").value,
+  };
+  emailjs
+    .send("service_tv0mk79", "template_4rihmoj", parms)
+    .then(alert("Form submitted successfully"));
+}
